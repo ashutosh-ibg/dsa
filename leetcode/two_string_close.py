583ee15cef4cc2ffc36d518ef092388dc087e8d8
ashutoshibg <ashutoshtech2017@gmail.com>
2024-01-14 22:33:08 +0530

    two_string_close

---
+++ leetcode/two_string_close.py
@@ -0,0 +1,47 @@
+# word1 = "abbzzca"
+# word2 = "babzzcz"
+
+
+# def closeStrings(word1, word2):
+#     if len(word1) != len(word2):
+#         return False
+
+#     word1_dic = {}
+#     for i in word1:
+#         if i not in word1_dic:
+#             word1_dic[i] = 1
+#         else:
+#             word1_dic[i] = word1_dic[i] + 1
+
+#     word2_dic = {}
+#     for i in word2:
+#         if i not in word2_dic:
+#             word2_dic[i] = 1
+#         else:
+#             word2_dic[i] = word2_dic[i] + 1
+
+
+#     word1_keys = word1_dic.values()
+#     word2_keys = word2_dic.values()
+#     print(word1_keys, word2_keys)
+#     for i in range(len(word1_dic)):
+#         if i not in word2_dic:
+#             return False
+
+#     for i in word2_dic:
+#         if i not in word1_dic:
+#             return False
+
+
+#     return True
+
+# xyz = closeStrings(word1, word2)
+# print(xyz)
+
+
+
+word1 = "cabbba"
+word2 = "abbccc"
+
+
+ 
